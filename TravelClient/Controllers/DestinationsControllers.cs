3d29cd1e2fd252a24b2e13d3a5e5f21fd6e14698
Jerrod Styrk <jstyrk@citadel.edu>
2021-01-20 16:17:54 -0800

    Add Views and Controllers for destination, need db to connect
    Co-authored-by: Garrett Brown <gman9mm@live.com>

--- TravelClient/Controllers/DestinationsControllers.cs
+++ TravelClient/Controllers/DestinationsControllers.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using TravelClient.Models;
 using TravelClient.ViewModels;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace TravelClient.Controllers
 {
@@ -19,5 +20,46 @@ namespace TravelClient.Controllers
             return View(allDestinations);
         }
 
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Destination destination)
+        {
+            _db.Destinations.Add(destination);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var thisDestination = _db.Destinations.FirstOrDefault(destination => destination.DestinationId == id);
+            return View(thisDestination);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Destination destination)
+        {
+            _db.Entry(destination).State = EntityState.Modified; 
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var thisDestination = _db.Destinations.FirstOrDefault(destination => destination.DestinationId == id);
+            return View(thisDestination);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var thisDestination = _db.Destinations.FirstOrDefault(destination => destination.DestinationId == id);
+            _db.Destinations.Remove(thisDestination);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
