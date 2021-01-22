44da35be0d2da01c05299881edfddce03f062f26
Jerrod Styrk <jstyrk@citadel.edu>
2021-01-21 16:18:33 -0800

    Add versioning
    Co-authored-by: Garrett Brown <gman9mm@live.com>

--- TravelAPI/Controllers/DestinationsController.cs
+++ TravelAPI/Controllers/DestinationsController.cs
@@ -6,73 +6,112 @@ using Microsoft.EntityFrameworkCore;
 
 namespace TravelAPI.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class DestinationsController : ControllerBase
+  [ApiVersion("1.0")]
+  [Route("api/destinations")]
+  [ApiController]
+  public class DestinationsV1Controller : ControllerBase
+  {
+    private TravelAPIContext _db;
+
+    public DestinationsV1Controller(TravelAPIContext db)
     {
-        private TravelAPIContext _db;
+      _db = db;
+    }
 
-        public DestinationsController(TravelAPIContext db)
+    // GET api/destinations
+    [HttpGet]
+    public ActionResult<IEnumerable<Destination>> Get(string country, string city, int rating)
+    {
+
+      var query = _db.Destinations.AsQueryable();
+
+      if (country != null)
+      {
+        query = query.Where(entry => entry.Country == country);
+      }
+
+      if (city != null)
+      {
+        query = query.Where(entry => entry.City == city);
+      }
+
+      if (rating != 0)
+      {
+        query = query.Where(entry => entry.Rating == rating);
+      }
+      return query.ToList();
+    }
+    //http://locahost:5000/api/destinations?api-version=2.0
+    [ApiVersion("2.0")]
+    [Route("api/{v:apiVersion}/destinations")]
+    [ApiController]
+    public class DestinationsV2Controller : ControllerBase
+    {
+      private TravelAPIContext _db;
+
+      public DestinationsV2Controller(TravelAPIContext db)
+      {
+        _db = db;
+      }
+
+      // GET api/destinations
+      [HttpGet]
+      public ActionResult<IEnumerable<Destination>> Get(string country, string city, int rating)
+      {
+
+        var query = _db.Destinations.AsQueryable();
+
+        if (country != null)
         {
-            _db = db;
+          query = query.Where(entry => entry.Country == country);
         }
 
-        // GET api/destinations
-        [HttpGet]
-        public ActionResult<IEnumerable<Destination>> Get(string country, string city, int rating)
+        if (city != null)
         {
-            
-            var query = _db.Destinations.AsQueryable();
-
-            if (country != null)
-            {
-                query = query.Where(entry => entry.Country == country);
-            }
-
-            if (city != null)
-            {
-                query = query.Where(entry => entry.City == city);
-            }
-
-            if (rating != 0)
-            {
-                query = query.Where(entry => entry.Rating == rating);
-            }
-            return query.ToList();
+          query = query.Where(entry => entry.City == city);
         }
 
-        // Post api/destinations
-        [HttpPost]
-        public void Post([FromBody] Destination destination)
+        if (rating != 0)
         {
-            _db.Destinations.Add(destination);
-            _db.SaveChanges();
+          query = query.Where(entry => entry.Rating == rating);
         }
-
-        // GET api/destination/id
-        [HttpGet("{id}")]
-        public ActionResult<Destination> Get(int id)
-        {
-            return _db.Destinations.FirstOrDefault(entry => entry.DestinationId == id);
-        }
-
-        //PUT api/destination/id
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] Destination destination)
-        {
-            destination.DestinationId = id;
-            _db.Entry(destination).State = EntityState.Modified;
-            _db.SaveChanges();
-        }
-        
-        // DELETE api/destinations/id
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-            var destinationToDelete = _db.Destinations.FirstOrDefault(entry => entry.DestinationId == id);
-            _db.Destinations.Remove(destinationToDelete);
-            _db.SaveChanges();
-        }
+        return query.ToList();
+      }
 
     }
+
+    // // Post api/destinations
+    // [HttpPost]
+    // public void Post([FromBody] Destination destination)
+    // {
+    //     _db.Destinations.Add(destination);
+    //     _db.SaveChanges();
+    // }
+
+    // // GET api/destination/id
+    // [HttpGet("{id}")]
+    // public ActionResult<Destination> Get(int id)
+    // {
+    //     return _db.Destinations.FirstOrDefault(entry => entry.DestinationId == id);
+    // }
+
+    // //PUT api/destination/id
+    // [HttpPut("{id}")]
+    // public void Put(int id, [FromBody] Destination destination)
+    // {
+    //     destination.DestinationId = id;
+    //     _db.Entry(destination).State = EntityState.Modified;
+    //     _db.SaveChanges();
+    // }
+
+    // // DELETE api/destinations/id
+    // [HttpDelete("{id}")]
+    // public void Delete(int id)
+    // {
+    //     var destinationToDelete = _db.Destinations.FirstOrDefault(entry => entry.DestinationId == id);
+    //     _db.Destinations.Remove(destinationToDelete);
+    //     _db.SaveChanges();
+    // }
+
+  }
 }
